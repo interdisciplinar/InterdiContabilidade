44d0e4e1bc59c625ed60d9954754aea94ecb537c
Vinicius <vinicavaco3@gmail.com>
2017-06-23 08:54:23 -0300

    Funcionalidades da aba de Ficha Técnica
    para mais detalhes: https://trello.com/c/sQ5s1Uht/6-funcionalidades-da-aba-de-ficha-t%C3%A9cnica

--- src/br/edu/unifebe/interdisciplinar/dao/FichaTecDao.java
+++ src/br/edu/unifebe/interdisciplinar/dao/FichaTecDao.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
+
+import br.edu.unifebe.interdisciplinar.Produto;
 import br.edu.unifebe.interdisciplinar.conexao.ConnectionDB;
 import br.edu.unifebe.interdisciplinar.model.CadFichaTec;
 
@@ -19,7 +21,21 @@ public class FichaTecDao implements IDao<CadFichaTec>{
 	
 	@Override
 	public boolean setIncluir(CadFichaTec e) throws SQLException {
-		return false;
+		String sql = "INSERT INTO `fichatecnica`(`ft_nome`, `ft_dt_criacao`, `ft_total_custo`, `ft_usu_id_criacao`) "
+				+ "VALUES (?,CURDATE(),?,?)";
+		PreparedStatement prmt;
+		try {
+			prmt = conexao.prepareStatement(sql);
+			prmt.setString(1, e.getNomeFicha());
+			prmt.setDouble(2, e.getTotalCusto());
+			prmt.setInt(3, 1);
+			prmt.executeUpdate();
+			return true;
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return false;
+		}
 	}
 
 	@Override
