2e140c86154558ac5b58680f546d37265c72d803
joserenatodesouza <joserenato@unifebe.edu.br>
2017-08-09 22:53:20 -0300

    Servidor Unifebe
    Corrigida a conexão para utilizar no servidor da Unifebe e comentada a conexão para utilização na máquina local

--- src/br/edu/unifebe/interdisciplinar/conexao/ConnectionDB.java
+++ src/br/edu/unifebe/interdisciplinar/conexao/ConnectionDB.java
@@ -17,53 +17,52 @@ public class ConnectionDB implements ConnectionDBInterface{
     private String PASS;
 	
 	public ConnectionDB() {
+		
 	        JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-	        DB_URL = "jdbc:mysql://localhost/intercontabilidade?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-	        USER = "root";
-	        PASS = "";
+	        
+//	        Conexão com o Banco para servidor Unifebe
+	        DB_URL = "jdbc:mysql://localhost/scpps?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	        USER = "scpps";
+	        PASS = "6t_N6hxOtB-lZO}l";
+	        
+//	        Conexão com o Banco para máquina local
+//	        DB_URL = "jdbc:mysql://localhost/intercontabilidade?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+//	        USER = "root";
+//	        PASS = "";
 	        conn = null;
-	        try{
+	        	        
+	        try {
 				/*Conectando ao Servidor de Banco de Dados*/
 				Class.forName(JDBC_DRIVER);
-				if(s == null){
+				if(s == null) {
 					System.out.print("Connecting to a selected database... ");
 		            conn = DriverManager.getConnection(DB_URL, USER, PASS);
 		            System.out.println("Success!");
-					/*conn = DriverManager.getConnection("jdbc:postgresql://localhost:5433/","postgres","postgres");//cria conexao com o servidor
-					s= conn.createStatement();
-			
-					conn = DriverManager.getConnection("jdbc:postgresql://localhost:5433/"+database,"postgres","postgres");//cria conexao com a base de dados criada
-					s= conn.createStatement(); //cria objeto Statement
-	*/			}
+				}
 			}
-			catch(Exception e){//excesao geral. Crie mais catchs do tipo SQLException etc para capturar exceaaes mais eficazmente
+			catch(Exception e) {
 		 		e.printStackTrace();
 			}
 	}
 	
-	public Connection getConnetion(){
+	public Connection getConnetion() {
 		return conn;
 	}
 	
 	
 	@Override
-	public void iniDB() throws SQLException{
-		if(conn == null){
-			try{
-				/*Conectando ao Servidor de Banco de Dados*/
+	public void iniDB() throws SQLException {
+		if(conn == null) {
+			try {
+				//Conectando ao Servidor de Banco de Dados
 				Class.forName(JDBC_DRIVER);
-				if(s == null){
+				if(s == null) {
 					//System.out.print("Connecting to a selected database... ");
 		            conn = DriverManager.getConnection(DB_URL, USER, PASS);
 		            System.out.println("Success!");
-					/*conn = DriverManager.getConnection("jdbc:postgresql://localhost:5433/","postgres","postgres");//cria conexao com o servidor
-					s= conn.createStatement();
-			
-					conn = DriverManager.getConnection("jdbc:postgresql://localhost:5433/"+database,"postgres","postgres");//cria conexao com a base de dados criada
-					s= conn.createStatement(); //cria objeto Statement
-	*/			}
+				}
 			}
-			catch(Exception e){//excesao geral. Crie mais catchs do tipo SQLException etc para capturar exceaaes mais eficazmente
+			catch(Exception e) {
 		 		e.printStackTrace();
 			}
 		}
